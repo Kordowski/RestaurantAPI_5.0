9aa39e1afe0f1b3617b2be076a4f962c28d8aa45
Kordowski <rafal@kordowski.pl>
2023-08-07 22:39:35 +0200

    Add project files.

---
+++ RestaurantAPI_5.0/IWeatherForecastService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace RestaurantAPI_5._0
+{
+    public interface IWeatherForecastService
+    {
+        IEnumerable<WeatherForecast> Get(int count, int minTemperature, int maxTemperature);
+    }
+}
---
+++ RestaurantAPI_5.0/TemperatureRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace RestaurantAPI_5._0
+{
+    public class TemperatureRequest
+    {
+        public int Min { get; set; }
+        public int Max { get; set; }
+        
+    }
+}
